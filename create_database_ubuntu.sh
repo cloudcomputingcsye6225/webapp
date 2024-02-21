62506231a6b958c8d02d44cbdd36f6736f4c16cc
root <GeneralMing>
2024-02-21 07:42:54 +0000

    added env variables to run

--- create_database_ubuntu.sh
+++ create_database_ubuntu.sh
@@ -2,7 +2,7 @@
 sudo systemctl start mysql
 
 # Create MySQL database and user
-sudo mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
-sudo mysql -u root -e "CREATE USER '$MYSQL_USER'@'$MYSQL_HOST' IDENTIFIED BY '$MYSQL_PASSWORD';"
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'$MYSQL_HOST';"
-sudo mysql -u root -e "FLUSH PRIVILEGES;"
+sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE;"
+sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER'@'$MYSQL_HOST' IDENTIFIED BY '$MYSQL_PASSWORD';"
+sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'$MYSQL_HOST';"
+sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
