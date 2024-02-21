3eb76c8b9dea83608dc4bf143a76d3586bc005b0
root <GeneralMing>
2024-02-21 07:20:52 +0000

    added env variables to run

--- update_libraries_ubuntu.sh
+++ update_libraries_ubuntu.sh
@@ -4,7 +4,7 @@
 sudo apt update -y
 
 # Upgrade system packages
-sudo apt upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 # Install required packages
-sudo apt install -y python3 mysql-server python3-pip virtualenv unzip gcc libmysqlclient-dev
+sudo DEBIAN_FRONTEND=noninteractive apt install -y python3 mysql-server python3-pip virtualenv unzip gcc libmysqlclient-dev
