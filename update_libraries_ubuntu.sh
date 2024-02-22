9bc05a2f6d5df2acf9ce8069b9d09d004b9a1f7b
GeneralMing <advaithkrishna7@gmail.com>
2024-02-21 22:40:17 -0500

    added build code

--- update_libraries_ubuntu.sh
+++ update_libraries_ubuntu.sh
@@ -7,4 +7,4 @@ sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 # Install required packages
-sudo DEBIAN_FRONTEND=noninteractive apt install -y python3 mysql-server python3-pip virtualenv unzip gcc libmysqlclient-dev
+sudo DEBIAN_FRONTEND=noninteractive apt install -y python3 mysql-server python3-pip virtualenv unzip gcc libmysqlclient-dev zip
