76f134d1a3278e193d9e5e966561ee7582ca22b3
GeneralMing <advaithkrishna7@gmail.com>
2024-03-20 15:00:11 -0400

    Added logs

--- utilities.py
+++ utilities.py
@@ -1,15 +1,19 @@
 import bcrypt
 import random
 import string
-
+from database_config import logger
 def hash_password(password):
+        logger.info("Hashing Password....", severity = "INFO")
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        logger.info("Password Hashed", severity = "INFO")
         return hashed_password
 
 def verify(hashed_password, password):
+        logger.info("Verifying password", severity = "INFO")
         return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 def generate_random_string(length):
+    logger.info("random string gen..", severity = "INFO")
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for _ in range(length))
